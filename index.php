4945e0edf82a1d9ca4b5f70a8a3c4fd56dfbd61a
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2018-03-15 17:58:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
 <?php
 $post_data = $_POST['message'];
-echo $post_data;
+//echo $post_data;
+
+$request = new HttpRequest();
+$request->setUrl('https://notify-api.line.me/api/notify');
+$request->setMethod(HTTP_METH_POST);
+
+$request->setHeaders(array(
+  'postman-token' => '1a6b5f80-9b69-62e9-e0ac-4de241d90879',
+  'cache-control' => 'no-cache',
+  'content-type' => 'application/x-www-form-urlencoded',
+  'authorization' => 'Bearer Zv3KWsygHysC16wv9igBc7JB0UgyU2hpX9sZsDYS9FT'
+));
+
+$request->setContentType('application/x-www-form-urlencoded');
+$request->setPostFields(array(
+  'message' => $post_data
+));
+
+try {
+  $response = $request->send();
+
+  echo $response->getBody();
+} catch (HttpException $ex) {
+  echo $ex;
+}
 ?>
