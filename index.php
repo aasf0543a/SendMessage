1db62504da8c90fc1c091858d74349e4dc9166c8
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2018-03-15 18:10:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
 <?php
 $post_data = $_POST['message'];
-//echo $post_data;
-
-$request = new HttpRequest();
-$request->setUrl('https://notify-api.line.me/api/notify');
-$request->setMethod(HTTP_METH_POST);
-
-$request->setHeaders(array(
-  'authorization' => 'Bearer Zv3KWsygHysC16wv9igBc7JB0UgyU2hpX9sZsDYS9FT',
-  'cache-control' => 'no-cache',
-  'content-type' => 'application/x-www-form-urlencoded'
-));
-
-$request->setContentType('application/x-www-form-urlencoded');
-$request->setPostFields(array(
-  'message' => $post_data
-));
-
-try {
-  $response = $request->send($post_data);
-  echo $response->getBody();
-}
+echo $post_data;
+?>
