f877e46d7c7870d9e54cd0ce2164ded7efcce2a4
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2018-03-15 18:00:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $request->setUrl('https://notify-api.line.me/api/notify');
 $request->setMethod(HTTP_METH_POST);
 
 $request->setHeaders(array(
-  'postman-token' => '1a6b5f80-9b69-62e9-e0ac-4de241d90879',
+  'postman-token' => '997280eb-dded-1202-2017-91216a9e2c87',
   'cache-control' => 'no-cache',
   'content-type' => 'application/x-www-form-urlencoded',
   'authorization' => 'Bearer Zv3KWsygHysC16wv9igBc7JB0UgyU2hpX9sZsDYS9FT'
