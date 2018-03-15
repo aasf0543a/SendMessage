00f7f8040d730f560255784db5639d364cec76ce
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2018-03-15 15:07:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,17 @@ $post_data = $_POST;
 var_dump($post_data);
 echo $post_data
 ?>
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<h1>echo $post_data</h1>
+
+<ul>
+    <li><a href="start_authorize.php">認証開始</a></li>
+</ul>
+</body>
+</html>
