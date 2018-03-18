11fdca5cb79a6c3f1d0148fa6299a43dd1908d1b
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2018-03-18 22:27:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,4 +6,38 @@ $post_data = $_POST;
 echo $post_data;
 //var_dump($post_data)
 
+<?php
+define('LINE_API_URL'  ,"https://notify-api.line.me/api/notify");
+define('LINE_API_TOKEN','トークン');
+
+function post_message($message){
+
+    $data = array(
+                        "message" => $message
+                     );
+    $data = http_build_query($data, "", "&");
+
+    $options = array(
+        'http'=>array(
+            'method'=>'POST',
+            'header'=>"Authorization: Bearer " . LINE_API_TOKEN . "\r\n"
+                      . "Content-Type: application/x-www-form-urlencoded\r\n"
+                      . "Content-Length: ".strlen($data)  . "\r\n" ,
+            'content' => $data
+        )
+    );
+    $context = stream_context_create($options);
+    $resultJson = file_get_contents(LINE_API_URL,FALSE,$context );
+    $resutlArray = json_decode($resultJson,TRUE);
+    if( $resutlArray['status'] != 200)  {
+        return false;
+    }
+    return true;
+}
+
+
+post_message("HIHIHI");
+
+
+
 ?>
