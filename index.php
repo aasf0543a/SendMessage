dc0896398e89b39b52e394b8973f8c880f21e1d1
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2018-03-18 21:58:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,25 +5,5 @@
 $post_data = $_POST;
 echo $post_data;
 //var_dump($post_data)
-$request->setUrl('http://notify-api.line.me/api/notify');
-$request->setMethod(HTTP_METH_POST);
 
-$request->setHeaders(array(
-  'Method' => 'POST',
-  'content-type' => 'application/x-www-form-urlencoded',
-  'authorization' => 'Bearer Zv3KWsygHysC16wv9igBc7JB0UgyU2hpX9sZsDYS9FT'
-));
-
-$request->setContentType('application/x-www-form-urlencoded');
-$request->setPostFields(array(
-  'message' => '好的老師帶你上天堂'
-));
-
-try {
-  $response = $request->send();
-
-  echo $response->getBody();
-} catch (HttpException $ex) {
-  echo $ex;
-}
 ?>
