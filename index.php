864b008ffcb2469d872a2443811fd81c5168d57f
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2018-03-18 22:14:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-//$post_data = &_GET['manid'];
+$post_data = &_GET['manid'];
 //print $post_data;
 //$post_data = file_get_contents("php://input");
-$post_data = $_POST;
+//$post_data = $_POST;
 echo $post_data;
 //var_dump($post_data)
 
