089bdd84311a03293757a9f5548ff6b484488cee
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2018-03-20 22:24:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 //$post_data = &_GET['manid'];
 //print $post_data;
-$post_data = file_get_contents("php://input");
+//$post_data = file_get_contents("php://input");
 //$post_data = $_POST;
 //var_dump($post_data);
-//$post_safety = $_POST['safety'];
-//$post_token = $_POST['manid'];
-//$post_char = $_POST['char'];
-//$post_backwork = $_POST['backwork'];
-//$post_manname = $_POST['manname'];
-//$post_mess = $post_char . $post_manname . $post_safety . $post_backwork;
+$post_safety = $_POST['safety'];
+$post_token = $_POST['manid'];
+$post_char = $_POST['char'];
+$post_backwork = $_POST['backwork'];
+$post_manname = $_POST['manname'];
+$post_mess = $post_char . $post_manname . $post_safety . $post_backwork;
 
 define('LINE_API_URL'  ,"https://notify-api.line.me/api/notify");
-define('LINE_API_TOKEN','Zv3KWsygHysC16wv9igBc7JB0UgyU2hpX9sZsDYS9FT');
+//define('LINE_API_TOKEN','Zv3KWsygHysC16wv9igBc7JB0UgyU2hpX9sZsDYS9FT');
 
 function post_message($message){
 
@@ -24,7 +24,7 @@ function post_message($message){
     $options = array(
         'http'=>array(
             'method'=>'POST',
-            'header'=>"Authorization: Bearer " . LINE_API_TOKEN . "\r\n"
+            'header'=>"Authorization: Bearer " . $post_token . "\r\n"
                       . "Content-Type: application/x-www-form-urlencoded\r\n"
                       . "Content-Length: ".strlen($data)  . "\r\n" ,
             'content' => $data
@@ -39,6 +39,6 @@ function post_message($message){
     return true;
 }
 
-post_message($post_data);
+post_message($post_mess);
 
 ?>
