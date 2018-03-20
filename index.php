483900e8d1243faf378c33bcabd8d878502041b0
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2018-03-20 21:57:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 //$post_data = &_GET['manid'];
 //print $post_data;
-//$post_data = file_get_contents("php://input");
+$post_data = file_get_contents("php://input");
 //$post_data = $_POST;
 //var_dump($post_data);
-$post_safety = $_POST['safety'];
+//$post_safety = $_POST['safety'];
 $post_token = $_POST['manid'];
-$post_char = $_POST['char'];
-$post_backwork = $_POST['backwork'];
-$post_manname = $_POST['manname'];
-$post_mess = $post_char . $post_manname . $post_safety . $post_backwork;
+//$post_char = $_POST['char'];
+//$post_backwork = $_POST['backwork'];
+//$post_manname = $_POST['manname'];
+//$post_mess = $post_char . $post_manname . $post_safety . $post_backwork;
 
 define('LINE_API_URL'  ,"https://notify-api.line.me/api/notify");
 //define('LINE_API_TOKEN','Zv3KWsygHysC16wv9igBc7JB0UgyU2hpX9sZsDYS9FT');
@@ -40,7 +40,7 @@ function post_message($message){
 }
 
 
-post_message($post_mess);
+post_message($post_token);
 
 
 
