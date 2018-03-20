af3a1106799a9f1f590e9b1496f9669b5bb19760
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2018-03-20 22:46:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,33 @@
 <?php
 //$post_data = &_GET['manid'];
 //print $post_data;
-//$post_data = file_get_contents("php://input");
+$post_data = file_get_contents("php://input");
+$data1 = array(
+    $post_data
+    );
+$mess = $data1[1] . $data1[2] . $data1[3] . $data1[4]
 //$post_data = $_POST;
 //var_dump($post_data);
-$post_safety = $_POST['safety'];
+//$post_safety = $_POST['safety'];
 //$post_token = $_POST['manid'];
-$post_char = $_POST['char'];
-$post_backwork = $_POST['backwork'];
-$post_manname = $_POST['manname'];
-$post_mess = $post_char . $post_manname . $post_safety . $post_backwork;
-
+//$post_char = $_POST['char'];
+//$post_backwork = $_POST['backwork'];
+//$post_manname = $_POST['manname'];
+//$post_mess = $post_char . $post_manname . $post_safety . $post_backwork;
 define('LINE_API_URL'  ,"https://notify-api.line.me/api/notify");
-define('LINE_API_TOKEN','Zv3KWsygHysC16wv9igBc7JB0UgyU2hpX9sZsDYS9FT');
+//define('LINE_API_TOKEN','Zv3KWsygHysC16wv9igBc7JB0UgyU2hpX9sZsDYS9FT');
 
-function post_message($message){
+function post_message($message1){
 
     $data = array(
-                        "message" => $message
+                        "message" => $message1//先將message轉成索引数组
                      );
-    $data = http_build_query($data, "", "&");
+    $data = http_build_query($data, "", "&");//再將轉成變數=Value&變數=Value
 
     $options = array(
         'http'=>array(
             'method'=>'POST',
-            'header'=>"Authorization: Bearer " . LINE_API_TOKEN . "\r\n"
+            'header'=>"Authorization: Bearer " . $data1[0] . "\r\n"
                       . "Content-Type: application/x-www-form-urlencoded\r\n"
                       . "Content-Length: ".strlen($data)  . "\r\n" ,
             'content' => $data
@@ -39,6 +42,6 @@ function post_message($message){
     return true;
 }
 
-post_message($post_mess);
+post_message($mess);
 
 ?>
