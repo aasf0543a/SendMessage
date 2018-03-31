1db1f8cf1ec34b648f06cb30891bb7c4907cb8c8
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2018-03-31 13:11:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $post_token = $_POST['token'];
 //$post_manname = $_POST['manname'];
 //$post_mess = $post_char . $post_manname . $post_safety . $post_backwork;
 define('LINE_API_URL'  ,"https://notify-api.line.me/api/notify");
-//define('LINE_API_TOKEN','Zv3KWsygHysC16wv9igBc7JB0UgyU2hpX9sZsDYS9FT');
+define('LINE_API_TOKEN','Zv3KWsygHysC16wv9igBc7JB0UgyU2hpX9sZsDYS9FT');
 
 function post_message($message){
 
@@ -30,7 +30,7 @@ function post_message($message){
     $options = array(
         'http'=>array(
             'method'=>'POST',
-            'header'=>"Authorization: Bearer " . $post_token . "\r\n"
+            'header'=>"Authorization: Bearer " . LINE_API_TOKEN . "\r\n"
                       . "Content-Type: application/x-www-form-urlencoded\r\n"
                       . "Content-Length: ".strlen($data)  . "\r\n" ,
             'content' => $data
