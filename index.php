2022eb7ceeaa213ccbadb1cb833c3014bb5cb1ba
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2018-03-31 13:31:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,21 +2,12 @@
 //example,
 //post數據= "username=" & 帳號 & "&password=" & 密碼 & "&qeustions........"
 //url = XXXXXXX?key=value&key=value
-//$post_data = &_GET['manid'];
-//print $post_data;
-//$post_data = file_get_contents("php://input");
-//$data1 = array(
-//    $post_data
-//    );
-//$mess = $data1[1] . $data1[2] . $data1[3] . $data1[4];
-//$post_data = $_POST;
-//var_dump($post_data);
-//$post_safety = $_POST['safety'];
+$post_safety = $_POST['safety'];
 $post_token = $_POST['token'];
-//$post_char = $_POST['char'];
-//$post_backwork = $_POST['backwork'];
-//$post_manname = $_POST['manname'];
-//$post_mess = $post_char . $post_manname . $post_safety . $post_backwork;
+$post_char = $_POST['char'];
+$post_backwork = $_POST['backwork'];
+$post_manname = $_POST['manname'];
+$post_mess = $post_char . "\r\n" . $post_manname . "\r\n" . $post_safety . "\r\n" . $post_backwork;
 define('LINE_API_URL'  ,"https://notify-api.line.me/api/notify");
 //define('LINE_API_TOKEN','Zv3KWsygHysC16wv9igBc7JB0UgyU2hpX9sZsDYS9FT');
 define('API_TOKEN', $post_token);
